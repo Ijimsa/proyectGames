1272187c486403f778e80b6a67873e4eb744f61e
alispozo <alvaroalis@Air-de-Alvaro.home>
2023-03-14 13:03:42 +0100

    Front login y register

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+import { UserService } from "../users/users.service";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
+})
+export class LoginComponent {
+  username: string | undefined;
+  password: string | undefined;
+
+  constructor(public userService: UserService) {}
+
+  login() {
+    const user = {username: this.username, password: this.password};
+    this.userService.login(user).subscribe( data => {
+      console.log(data);
+    });
+  }
+}
